1c6c42742fb11066418a75a9d8a9f08c7e2ac14f
diwakar8686 <diw2akaraubr@gmail.com>
2024-12-13 19:54:25 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function extractAndReverse(arr) {
+    const subarray = arr.slice(2, 5);
+   const reversedSubarray = subarray.reverse();
+       return reversedSubarray;
+   }
+   const originalArray = [15, 30, 45, 60, 75, 90];
+   const result = extractAndReverse(originalArray);
+   
+   console.log("Original Array:", originalArray); 
+   console.log("Reversed Subarray:", result);    
+   
